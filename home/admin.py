a761c34d779f56579db439054cea447dbd47e4b3
vinitkumargole <77961291+vinitkumargole@users.noreply.github.com>
2023-11-07 15:32:17 +0530

    done final

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from home.models import Contact
+from home.models import Contact,Enroll
 
 # Register your models here.
 
 admin.site.register(Contact)
+admin.site.register(Enroll)
+
